e2b309eeb109665e440b51675762cea048b90324
RAJAHKOV <vorwei00@gmail.com>
2026-09-20 14:30:03 +0800

    20260920

--- test2.cpp
+++ test2.cpp
@@ -6,6 +6,13 @@ int main(){
     cin >> a >> b;
     cout<<"a+b="<< a+b<<endl;
     cout<<"a*b="<<a*b<<endl;
-
+    cout << "a-b=" << a-b << endl;
+    if (b!=0)
+    {
+        cout << "a/b=" << a/b << endl;
+    }
+    else{
+        cout << "devider can't be zero" << endl;
+    }
     return 0;
 }
